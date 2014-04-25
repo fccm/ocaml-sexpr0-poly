b50a617b4189d41aa541b27e77519dc345e339a6
Blue Prawn <monnier.florent@gmail.com>
2014-04-25 14:11:57 +0159

    also produce .cma and .cmxa files

--- SExpr.mk
+++ SExpr.mk
@@ -2,7 +2,10 @@ OCAML = ocaml
 OCAMLC = ocamlc
 OCAMLOPT = ocamlopt
 
-all: SExpr.cmo
+all: lib opt
+.PHONY: lib opt
+lib: SExpr.cma
+opt: SExpr.cmxa
 
 SExpr.cmi: SExpr.mli
 	$(OCAMLC) -c $<
@@ -10,9 +13,16 @@ SExpr.cmo: SExpr.ml SExpr.cmi
 	$(OCAMLC) -c $<
 SExpr.cmx: SExpr.ml SExpr.cmi
 	$(OCAMLOPT) -c $<
+
+SExpr.cma: SExpr.cmo
+	$(OCAMLC) -a -o $@ $<
+SExpr.cmxa: SExpr.cmx
+	$(OCAMLOPT) -a -o $@ $<
+
 test: SExpr.cmo
 	$(OCAML) SExpr.cmo SExpr_test.ml
 clean:
 	$(RM) SExpr.[oa] SExpr.cm[ioxa] SExpr.cmx[as]
 
-.PHONY: all test clean
+.PHONY: test clean
+
